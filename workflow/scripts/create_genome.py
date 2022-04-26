a5052a99bfea39c93943b55eff90806ff62d1bad
Nancy F. Hansen <nhansen@mail.nih.gov>
2022-04-26 15:08:45 -0400

    General improvement

--- workflow/scripts/create_genome.py
+++ workflow/scripts/create_genome.py
@@ -1,8 +1,17 @@
 import os
+import re
 
 genome_file = snakemake.output[0]
-assembly_dir = "/cluster/ifs/projects/AlphaThal/haplotype_assemblies/"
-syscommand = "gunzip -c " + assembly_dir + "HG01109.maternal.f1_assembly_v2.fa.gz " + assembly_dir + "HG01109.paternal.f1_assembly_v2.fa.gz > " + genome_file
+match = re.search(r'^genomes/(\S{2})\_(\S{4})\_(\S{2})\_(\S{4})\.fasta$', genome_file)
+if match:
+    genome1 = snakemake.config["genomefiles"][match.group(1)]
+    genome2 = snakemake.config["genomefiles"][match.group(3)]
+    print('found', match.group(1), match.group(2), match.group(3), match.group(4))
+else:
+    print("Couldn\'t parse filename " + genome_file)
+
+assembly_dir = snakemake.config["genomedir"] + "/"
+syscommand = "gunzip -c " + assembly_dir + genome1 + " " + assembly_dir + genome2 + " > " + genome_file
 print(syscommand)
 os.system(syscommand)
 
